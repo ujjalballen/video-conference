cf2db77840264ed94fb9ff464d79374c378aa0f6
developerujjal <ujjal.vision02@gmail.com>
2025-05-05 14:17:06 +0600

    createRouter

--- basics/config/config.js
+++ basics/config/config.js
@@ -18,7 +18,34 @@ const config = {
             'rtcp'
         ]
 
-    }
+    },
+    routerMediaCodecs: [
+        {
+            kind: "audio",
+            mimeType: "audio/opus",
+            clockRate: 48000,
+            channels: 2
+        },
+        {
+            kind: "video",
+            mimeType: "video/H264",
+            clockRate: 90000,
+            parameters:
+            {
+                "packetization-mode": 1,
+                "profile-level-id": "42e01f",
+                "level-asymmetry-allowed": 1
+            }
+        },
+        {
+            kind: "video",
+            mimeType: "video/VP8",
+            clockRate: 90000,
+            parameters: {}
+        }
+    ],
+
+
 
 };
 
--- basics/server.js
+++ basics/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mediasoup = require('mediasoup');
-const {createServer } = require('http');
+const { createServer } = require('http');
 const { Server } = require('socket.io');
 const config = require('./config/config');
 const createWorkers = require('./createWorkers');
@@ -18,12 +18,21 @@ const io = new Server(httpServer, {
   }
 });
 
+// our global
 // init workers, it's where our mediasoup workers will live
 let workers = null;
 
-const initMediasoup = async() => {
+//init router, its where only 1 router will live
+let router = null;
+
+const initMediasoup = async () => {
   workers = await createWorkers(); // function call
-  console.log(workers)
+  // console.log(workers)
+
+  // we just take only one workers ==> we can run a forEach loop for all workers
+  router = await workers[0].createRouter({
+    mediaCodecs: config.routerMediaCodecs
+  })
 };
 
 initMediasoup(); // build our mediasoup server/sfu
@@ -31,5 +40,5 @@ initMediasoup(); // build our mediasoup server/sfu
 
 
 httpServer.listen(config.port, () => {
-    console.log(`listening on *:${config.port}`);
+  console.log(`listening on *:${config.port}`);
 })
