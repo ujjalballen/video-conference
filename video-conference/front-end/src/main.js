03172f311e8653c97ae9cc8318a190b8f81f5c42
developerujjal <ujjal.vision02@gmail.com>
2025-05-18 02:41:19 +0600

    Load device on front-end

--- video-conference/front-end/src/main.js
+++ video-conference/front-end/src/main.js
@@ -2,17 +2,29 @@ import './style.css'
 import button from '../uiStuff/uiButtons';
 import { Device, Transport } from 'mediasoup-client';
 import { io } from "socket.io-client";
+import uiButtons from '../uiStuff/uiButtons';
+
+let device = null;
 
 const socket = io('http://localhost:3031'); // server URL
 socket.on('connect', () => {
   console.log('socket connected!')
 });
 
-const joinRoom = async() => {
+const joinRoom = async () => {
   const userName = document.getElementById('username').value;
   const roomName = document.getElementById('room-input').value;
-  const joinRoomResp = await socket.emitWithAck('joinRoom', {userName, roomName});
-  console.log(joinRoomResp);
+  const joinRoomResp = await socket.emitWithAck('joinRoom', { userName, roomName });
+  // console.log(joinRoomResp);
+
+  device = new Device();
+
+  await device.load({ routerRtpCapabilities: joinRoomResp.routerRtpCapabilities });
+
+  console.log(device)
+
+  // PLACEHOLDER... Start making the transports for current speakers
+  uiButtons.control.classList.remove('d-none');
 };
 
 button.joinRoom.addEventListener('click', joinRoom);
